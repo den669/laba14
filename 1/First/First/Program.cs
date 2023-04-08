f2403bc84853405c31082f050687ec383125c229
den669 <71308629+den669@users.noreply.github.com>
2023-04-09 00:20:22 +0300

    Add files via upload

---
+++ 1/First/First/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadExample
+{
+    class Program
+    {
+        static int[] arr; 
+        static int minValue; 
+        static int maxValue; 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть розмір масиву:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            arr = new int[size];
+
+            Console.WriteLine("Введіть елементи масиву:");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Елемент {i + 1}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            
+            Thread threadMax = new Thread(FindMax);
+            
+            Thread threadMin = new Thread(FindMin);
+
+            
+            threadMax.Start();
+            threadMin.Start();
+
+            
+            threadMax.Join();
+            threadMin.Join();
+
+            
+            Console.WriteLine($"Максимум: {maxValue}");
+            Console.WriteLine($"Мінімум: {minValue}");
+        }
+
+        static void FindMax()
+        {
+            
+            maxValue = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > maxValue)
+                {
+                    maxValue = arr[i];
+                }
+            }
+        }
+
+        static void FindMin()
+        {
+            
+            minValue = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < minValue)
+                {
+                    minValue = arr[i];
+                }
+            }
+        }
+    }
+}
---
+++ 3/Third/Third/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileReadingExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] fileNames = { "AI1.txt", "AI2.txt", "AI3.txt" };
+
+            foreach (var fileName in fileNames)
+            {
+                await Task.Run(() => ReadFile(fileName));
+            }
+
+            Console.WriteLine("File reading completed.");
+        }
+
+        static void ReadFile(string fileName)
+        {
+            using (var streamReader = new StreamReader(fileName))
+            {
+                int N = 0;
+                if (int.TryParse(streamReader.ReadLine(), out N))
+                {
+                    Console.WriteLine($"File: {fileName}, N: {N}");
+                    for (int i = 0; i < N; i++)
+                    {
+                        if (int.TryParse(streamReader.ReadLine(), out int number))
+                        {
+                            Console.WriteLine($"File: {fileName}, Number: {number}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
